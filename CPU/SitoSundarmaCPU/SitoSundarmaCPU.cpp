79af569cf0b4f6c6d9c4859fd4d80b98b77ca749
Matyjash <lengom4w@gmail.com>
2022-11-02 20:49:16 +0100

    First CPU implementation

--- CPU/SitoSundarmaCPU/SitoSundarmaCPU.cpp
+++ CPU/SitoSundarmaCPU/SitoSundarmaCPU.cpp
@@ -1,7 +1,41 @@
 ﻿
 #include <iostream>
 
+void sieveOfSundaram(int n) {
+    
+    int k = (n - 2) / 2;
+    bool* numberArray = new bool[k + 1];
+    for (int i = 0; i < k + 1; i++)
+    {
+        numberArray[i] = true;
+    }
+
+    for (int i = 1; i < k + 1; i++) 
+    {
+        int j = i;
+        while (i + j + 2 * i * j <= k) 
+        {
+            numberArray[i + j + 2 * i * j] = false;
+            j++;
+        }
+    }
+    if (n > 2)
+    {
+        std::cout << 2 << " ";
+    }
+    for (int i = 1; i < k + 1; i++)
+    {
+        if (numberArray[i])
+        {
+            std::cout << 2 * i + 1 << " ";
+        }
+    }
+    delete[] numberArray;
+    return;
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
+    sieveOfSundaram(31);
 }
